92fdf3df0d70c7a8c92bb6a354fd4b85948d7cc4
Nick Hamnett <nick.hamnett@sait.ca>
2023-03-13 21:48:20 -0600

    Add project files.

---
+++ SerializationDemoE/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+
+namespace SerializationDemoE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Writing (serialization):
+
+            Student student1 = new Student("Joe Blow", 3.65, "password");
+            Student student2 = new Student("John Smith", 2.5, "secret");
+            Student student3 = new Student("Joe Schmoe", 3.9, "idaho");
+
+            /*FileStream fileStream = new FileStream("students.bin", FileMode.Truncate);
+
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            binaryFormatter.Serialize(fileStream, student1);
+            binaryFormatter.Serialize(fileStream, student2);
+            binaryFormatter.Serialize(fileStream, student3);
+
+            fileStream.Close();
+
+            Console.WriteLine("Saved object to file");*/
+
+            // Writing (JSON):
+            /*FileStream fileStream = new FileStream("students.json", FileMode.OpenOrCreate);
+
+            Student[] students = new Student[] { student1, student2, student3 };
+
+            JsonSerializer.Serialize(fileStream, students);
+
+            fileStream.Close();*/
+
+            // Reading (serialization):
+            /*FileStream fileStreamRead = File.OpenRead("students.bin");
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            while (fileStreamRead.Position < fileStreamRead.Length)
+            {
+                Student student = (Student)formatter.Deserialize(fileStreamRead);
+
+                Console.WriteLine(student.Name);
+            }
+            
+            fileStreamRead.Close();*/
+        }
+    }
+}
---
+++ SerializationDemoE/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SerializationDemoE
+{
+    [Serializable]
+    internal class Student
+    {
+        private string _name;
+        private double _gpa;
+        [NonSerialized]
+        private string _password;
+        private string _email;
+
+        public string Name { get { return _name; } }
+        public double GPA { get { return _gpa; } }
+        public string Password { get { return _password; } }
+
+        public Student()
+        {
+
+        }
+
+        public Student(string name, double gpa, string password)
+        {
+            _name = name;
+            _gpa = gpa;
+            _password = password;
+        }
+    }
+}
